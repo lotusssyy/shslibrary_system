ab995e84d5e47c674c0a140e1800f27ebab5cee7
Lotus <libraryuclm@gmail.com>
2025-03-02 11:39:16 +0800

    Updated db.php to use JawsDB MySQL

--- includes/db.php
+++ includes/db.php
@@ -1,12 +1,13 @@
 <?php
-$host = 'localhost';
-$db = 'library_management';
-$user = 'root';
-$pass = ''; // Leave blank for XAMPP
+$url = parse_url(getenv("JAWSDB_URL"));
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$dbname = substr($url["path"], 1);
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Database connection failed: " . $e->getMessage());
+    die("Connection failed: " . $e->getMessage());
 }
 ?>
